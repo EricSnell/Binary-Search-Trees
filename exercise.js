5335c6b59b3bfe91feaa34d0dd34bc87a11bdcb7
Sean Jun <seanjun21@gmail.com>
2016-08-23 12:47:02 -0700

    Exercise understanding completed

--- exercise.js
+++ exercise.js
@@ -30,3 +30,104 @@ BinarySearchTree.prototype.insert = function(key, value) {
 		}
 	}
 };
+
+BinarySearchTree.prototype.get = function(key) {
+	// If the current node's key equals search key
+	if (this.key === key) {
+		return this.value;
+	}
+	// if the search key's less than current key (and left exists)
+	else if (key < this.key && this.left) {
+		return this.left.get(key);
+	}
+	// if the search key's greater than current key (and right exists)
+	else if (key > this.key && this.right) {
+		return this.right.get(key);
+	}
+	// if all other cases (key doesn't exist) throw an error
+	else {
+		throw new Error('Key Error');
+	}
+};
+
+BinarySearchTree.prototype.remove = function(key) {
+	// When you have navigated to the node you want to remove
+	if (this.key === key) {
+		// If the node has both a left and a right child
+		if (this.left && this.right) {
+			// find the node containing the lowest key on the right branch
+			var successor = this.right._findMin();
+			this.key = successor.key;
+			this.value = successor.value;
+			successor.remove(successor.key);
+		} 
+		// If the node only has a left child, then replace with its left child
+		else if (this.left) {
+			this._replaceWith(this.left);
+		} 
+		// If the node only has a right child, then replace with its right child
+		else if (this.right) {
+			this._replaceWith(this.right);
+		} 
+		// If the node has no children then you simply remove it
+		else {
+			this._replaceWith(null);
+		}
+	}
+	// If the key you are looking to remove is less than the node's key
+	else if (key < this.key && this.left) {
+		this.left.remove(key);
+	} 
+	// If the key you are looking to remove is greater than the node's key
+	else if (key > this.key && this.right) {
+		this.right.remove(key);
+	} 
+	// if all other cases (key doesn't exist) throw an error
+	else {
+		throw new Error('Key Error');
+	}
+};
+
+BinarySearchTree.prototype._replaceWith = function(node) {
+	// If the node you are replacing has a parent
+	if (this.parent) {
+		// fix up the references from the parent to the replacement node
+		if (this === this.parent.left) {
+			this.parent.left = node;
+		}
+		// fix up the references from the parent to the replacement node
+		else if (this === this.parent.right) {
+			this.parent.right = node;
+		}
+		// and the replacement node back to the parent.
+		if (node) {
+			node.parent = this.parent;
+		}
+	}
+	// If the node is a root node
+	else {
+		// node does exist
+		if (node) {
+			// copy over the properties of the replacement node.
+			this.key = node.key;
+			this.value = node.value;
+			this.left = node.left;
+			this.right = node.right;
+		}
+		// node doesn't exist (node === null)
+		else {
+			this.key = null;
+			this.value = null;
+			this.left = null;
+			this.right = null;
+		}
+	}
+};
+
+BinarySearchTree.prototype._findMin = function() {
+	// You simply keep going left until you can go no further left.
+	if (!this.left) {
+		return this;
+	}
+	return this.left._findMin();
+};
